6eefc805a59281b7ea2653cdefd363066152f6a3
OSpodaryk <oksspodaryk@gmail.com>
2023-04-29 22:09:03 +0300

    Income

--- api/shoes_api/sql/functions/function.sql
+++ api/shoes_api/sql/functions/function.sql
@@ -137,3 +137,42 @@ RETURN
         SUM(o.TotalPrice - o.TotalPriceWithDiscount) AS DiscountLosses
     FROM Orders o
     GROUP BY YEAR(o.CreateDate);
+CREATE FUNCTION GetIncomeByYearAndSeasons(@Year INT)
+RETURNS TABLE
+AS
+RETURN
+    SELECT
+        @Year AS Year,
+        (SELECT SUM(TotalPrice) FROM Orders WHERE YEAR(CreateDate) = @Year AND MONTH(CreateDate) BETWEEN 1 AND 3) AS Season1Income,
+        (SELECT SUM(TotalPrice) FROM Orders WHERE YEAR(CreateDate) = @Year AND MONTH(CreateDate) BETWEEN 4 AND 6) AS Season2Income,
+        (SELECT SUM(TotalPrice) FROM Orders WHERE YEAR(CreateDate) = @Year AND MONTH(CreateDate) BETWEEN 7 AND 9) AS Season3Income,
+        (SELECT SUM(TotalPrice) FROM Orders WHERE YEAR(CreateDate) = @Year AND MONTH(CreateDate) BETWEEN 10 AND 12) AS Season4Income;
+CREATE FUNCTION GetIncomeByEveryMonth(@Year INT)
+RETURNS TABLE
+AS
+RETURN
+    SELECT
+        MonthNumber.Month,
+        ISNULL(SUM(OrdersIncome.Income), 0) AS Income
+    FROM (
+        VALUES (1), (2), (3), (4), (5), (6), (7), (8), (9), (10), (11), (12)
+    ) AS MonthNumber(Month)
+    LEFT JOIN (
+        SELECT
+            MONTH(CreateDate) AS Month,
+            SUM(TotalPrice) AS Income
+        FROM Orders
+        WHERE YEAR(CreateDate) = @Year
+        GROUP BY MONTH(CreateDate)
+    ) AS OrdersIncome ON MonthNumber.Month = OrdersIncome.Month
+    ORDER BY MonthNumber.Month;
+
+CREATE FUNCTION GetIncomeByYear()
+RETURNS TABLE
+AS
+RETURN
+    SELECT
+        YEAR(o.CreateDate) AS Year,
+        SUM(o.TotalPrice) AS Income
+    FROM Orders o
+    GROUP BY YEAR(o.CreateDate);
